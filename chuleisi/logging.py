a141f82a1dddf5897b8762e68d960fac7c026bda
Mathieu Bridon <bochecha@daitauha.fr>
2019-05-22 11:35:46 +0200

    Set up basic logging

---
+++ chuleisi/logging.py
@@ -0,0 +1,42 @@
+import logging
+import sys
+
+
+class StdoutFilter(logging.Filter):
+    def filter(self, record: logging.LogRecord) -> bool:
+        return record.levelno <= logging.INFO
+
+
+def _reset_logging() -> None:
+    # This is necessary for 2 reasons:
+    #
+    # 1.  pytest sets up the logging system (to capture logs) which means when setup_logging is
+    #     called its setup is ignored;
+    # 2.  after calling setup_logging once, calling a second time doesn't do anything either;
+    #
+    # Both issues make it impossible to properly test the setup_logging function without resetting
+    # the log setup.
+    #
+    # As this undoes the logging setup pytest does though, it means we can't use their caplog
+    # fixture. Instead, log messages are sent to their capsys/capfd fixtures.
+    root = logging.getLogger()
+
+    for handler in root.handlers[:]:
+        handler.close()
+        root.removeHandler(handler)
+
+
+def setup_logging(*, verbose: bool = False) -> None:
+    _reset_logging()
+
+    level = logging.DEBUG if verbose else logging.INFO
+
+    # Send DEBUG and INFO to stdout, WARNING and ERROR to stderr
+    out = logging.StreamHandler(stream=sys.stdout)
+    out.setLevel(logging.DEBUG)
+    out.addFilter(StdoutFilter())
+    err = logging.StreamHandler(stream=sys.stderr)
+    err.setLevel(logging.WARNING)
+
+    format_ = '[{levelname}] {name}: {message}'
+    logging.basicConfig(level=level, handlers=[out, err], format=format_, style='{')
---
+++ chuleisi/tests/test_logging.py
@@ -0,0 +1,56 @@
+import logging
+
+from chuleisi.logging import setup_logging
+
+
+def test_default(capfd):
+    setup_logging(verbose=False)
+    log = logging.getLogger(__name__)
+
+    log.debug('A debugging message')
+    log.info('An informative message')
+    log.warning('A scary warning')
+    log.error('Oh no!')
+
+    capture = capfd.readouterr()
+    assert 'A debugging message' not in capture.out
+    assert 'An informative message' in capture.out
+    assert 'A scary warning' in capture.err
+    assert 'Oh no!' in capture.err
+
+
+def test_verbose(capfd):
+    setup_logging(verbose=True)
+    log = logging.getLogger(__name__)
+
+    log.debug('A debugging message')
+    log.info('An informative message')
+    log.warning('A scary warning')
+    log.error('Oh no!')
+
+    capture = capfd.readouterr()
+    assert 'A debugging message' in capture.out
+    assert 'An informative message' in capture.out
+    assert 'A scary warning' in capture.err
+    assert 'Oh no!' in capture.err
+
+
+def test_reset_logging(capfd):
+    setup_logging(verbose=False)
+    log = logging.getLogger(__name__)
+
+    log.debug('A debugging message')
+    capture = capfd.readouterr()
+    assert 'A debugging message' not in capture.out
+
+    setup_logging(verbose=True)
+
+    log.debug('A debugging message')
+    capture = capfd.readouterr()
+    assert 'A debugging message' in capture.out
+
+    setup_logging(verbose=False)
+
+    log.debug('A debugging message')
+    capture = capfd.readouterr()
+    assert 'A debugging message' not in capture.out
