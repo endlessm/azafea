4e244e7b3d7072a2c60f98254601bd7b65d69ef6
Mathieu Bridon <bochecha@daitauha.fr>
2019-05-27 13:33:31 +0200

    processor: Pull events to process from  Redis
    This lays the ground work for pulling events from the various queues,
    before actually processing them.

--- chuleisi/processor.py
+++ chuleisi/processor.py
@@ -1,10 +1,9 @@
 import logging
 from multiprocessing import Process
-from random import randint
 from signal import SIGINT, SIGTERM, Signals, signal as intercept_signal
-import time
 from typing import Any
 
+from redis import Redis
 
 from .config import Config
 
@@ -19,6 +18,8 @@ class Processor(Process):
         self.config = config
         self._continue = True
 
+        self._redis = Redis(host=config.redis.host, port=config.redis.port)
+
     def _exit_cleanly(self, signum: int, _: Any) -> None:
         signal_name = Signals(signum).name
         log.info('{%s} Received %s, finishing the current task…', self.name, signal_name)
@@ -30,6 +31,16 @@ class Processor(Process):
         intercept_signal(SIGINT, self._exit_cleanly)
         intercept_signal(SIGTERM, self._exit_cleanly)
 
+        queues = ('some_events:1', 'other_events:1', 'other_events:2')
+
         while self._continue:
-            log.debug('{%s} I am doing some work!', self.name)
-            time.sleep(randint(0, 2))
+            result = self._redis.brpop(queues, timeout=5)
+
+            if result is None:
+                continue
+
+            queue, value = result
+            queue = queue.decode('utf-8')
+            value = value.decode('utf-8')
+
+            log.debug('{%s} Pulled "%s" from the %s queue', self.name, value, queue)
--- chuleisi/tests/test_processor.py
+++ chuleisi/tests/test_processor.py
@@ -1,10 +1,40 @@
 import os
 from signal import SIGINT, SIGTERM
 import time
+from typing import Optional, Sequence, Tuple
 
 from chuleisi.config import Config
 from chuleisi.logging import setup_logging
 import chuleisi.processor
+
+
+class MockRedis:
+    def __init__(self, host: str, port: int):
+        print('Created Redis client')
+
+        # FIXME: This is a hack just to have code coverage of the cases BRPOP returns None (the
+        # timeout was reached) as well as when it returns a value. Figure out a way to test this
+        # properly.
+        self._next_key = 0
+        self._next_value = 0
+
+    def brpop(self, keys: Sequence[str], timeout: int = 0) -> Optional[Tuple[bytes, bytes]]:
+        str_keys = ' '.join(keys)
+        print(f'Ran Redis command: BRPOP {str_keys}')
+
+        value = (None, b'value')[self._next_value]
+        self._next_value = (self._next_value + 1) % 2
+
+        if value is None:
+            return None
+
+        # Note: this only works if the number of queues doesn't change after having created the
+        # Redis client. In this application the configuration is only loaded at startup so this is
+        # a fair assumption.
+        key = keys[self._next_key]
+        self._next_key = (self._next_key + 1) % len(keys)
+
+        return key.encode('utf-8'), value
 
 
 def test_start(capfd):
@@ -23,12 +53,14 @@ def test_start(capfd):
     assert '{test-worker} Starting' in capture.out
 
 
-def test_start_then_sigint(capfd, make_config):
+def test_start_then_sigint(capfd, monkeypatch, make_config):
     config = make_config({'main': {'verbose': True}})
     setup_logging(verbose=config.main.verbose)
 
-    proc = chuleisi.processor.Processor('test-worker', config)
-    proc.start()
+    with monkeypatch.context() as m:
+        m.setattr(chuleisi.processor, 'Redis', MockRedis)
+        proc = chuleisi.processor.Processor('test-worker', config)
+        proc.start()
 
     # Give the process a bit of time to start before sending the signal; 0.1s should be way enough
     # to pass at least once in the main loop
@@ -39,16 +71,18 @@ def test_start_then_sigint(capfd, make_config):
 
     capture = capfd.readouterr()
     assert '{test-worker} Starting' in capture.out
-    assert '{test-worker} I am doing some work!' in capture.out
+    assert '{test-worker} Pulled "value" from the ' in capture.out
     assert '{test-worker} Received SIGINT, finishing the current task…' in capture.out
 
 
-def test_start_then_sigterm(capfd, make_config):
+def test_start_then_sigterm(capfd, monkeypatch, make_config):
     config = make_config({'main': {'verbose': True}})
     setup_logging(verbose=config.main.verbose)
 
-    proc = chuleisi.processor.Processor('test-worker', config)
-    proc.start()
+    with monkeypatch.context() as m:
+        m.setattr(chuleisi.processor, 'Redis', MockRedis)
+        proc = chuleisi.processor.Processor('test-worker', config)
+        proc.start()
 
     # Give the process a bit of time to start before sending the signal; 0.1s should be way enough
     # to pass at least once in the main loop
@@ -59,5 +93,5 @@ def test_start_then_sigterm(capfd, make_config):
 
     capture = capfd.readouterr()
     assert '{test-worker} Starting' in capture.out
-    assert '{test-worker} I am doing some work!' in capture.out
+    assert '{test-worker} Pulled "value" from the ' in capture.out
     assert '{test-worker} Received SIGTERM, finishing the current task…' in capture.out
