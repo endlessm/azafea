7ce824534b098d89e282db137b1e2c8e11311689
Mathieu Bridon <mathieu@hashbang.fr>
2019-06-07 13:22:38 +0200

    utils: Add a helper to get the FQDN of a callable
    This is useful to debug print functions and classes.

--- chuleisi/tests/test_utils.py
+++ chuleisi/tests/test_utils.py
@@ -1,3 +1,5 @@
+import pytest
+
 import chuleisi.utils
 
 
@@ -9,3 +11,17 @@ def test_get_cpu_count_failed(monkeypatch):
     with monkeypatch.context() as m:
         m.setattr(chuleisi.utils.os, 'cpu_count', mock_cpu_count)
         assert chuleisi.utils.get_cpu_count() == 1
+
+
+@pytest.mark.parametrize('module, name, expected', [
+    pytest.param('chuleisi.logging', 'setup_logging', 'chuleisi.logging.setup_logging',
+                 id='chuleisi.logging.setup_logging'),
+    pytest.param('chuleisi.controller', 'Controller', 'chuleisi.controller.Controller',
+                 id='chuleisi.controller.Controller'),
+])
+def test_get_fqdn(module, name, expected):
+    from importlib import import_module
+
+    module = import_module(module)
+    f = getattr(module, name)
+    assert chuleisi.utils.get_fqdn(f) == expected
--- chuleisi/utils.py
+++ chuleisi/utils.py
@@ -1,4 +1,5 @@
 import os
+from typing import Callable
 
 
 # The Python documentation for os.cpu_count() says:
@@ -8,3 +9,7 @@ import os
 # This makes sure to always return an integer.
 def get_cpu_count() -> int:
     return os.cpu_count() or 1
+
+
+def get_fqdn(f: Callable) -> str:
+    return f'{f.__module__}.{f.__name__}'
