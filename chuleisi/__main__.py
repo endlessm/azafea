0b533a2a7b0309805b10a1deb68f310353b14243
Mathieu Bridon <mathieu@hashbang.fr>
2019-06-11 15:39:27 +0200

    cli: Let the admin specify their configuration file

--- chuleisi/__main__.py
+++ chuleisi/__main__.py
@@ -1,11 +1,18 @@
+import argparse
 import sys
 
 from .config import Config, InvalidConfigurationError
 from .controller import Controller
 
 
+parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+parser.add_argument('-c', '--config', default='/etc/chuleisi/config.toml',
+                    help='Optional path to a configuration file, if needed')
+
+args = parser.parse_args()
+
 try:
-    config = Config.from_file('/etc/chuleisi/config.toml')
+    config = Config.from_file(args.config)
 
 except InvalidConfigurationError as e:
     sys.exit(str(e))
