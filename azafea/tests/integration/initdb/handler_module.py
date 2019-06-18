7e1d2b924d4b3f6875ff5135cb9b40eb7d4a5d4a
Mathieu Bridon <mathieu@hashbang.fr>
2019-06-18 13:52:17 +0200

    integration-tests: Ensure that initdb works

---
+++ azafea/tests/integration/initdb/handler_module.py
@@ -0,0 +1,16 @@
+from sqlalchemy.orm.session import Session as DbSession
+from sqlalchemy.schema import Column
+from sqlalchemy.types import Integer, Unicode
+
+from azafea.model import Base
+
+
+class Event(Base):
+    __tablename__ = 'event'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(Unicode, nullable=False)
+
+
+def process(dbsession: DbSession, record: bytes) -> None:
+    pass
