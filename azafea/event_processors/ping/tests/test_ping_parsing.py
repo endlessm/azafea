caa6505f9f2aa9fd74478bbff693f14fa491a8dc
Mathieu Bridon <mathieu@hashbang.fr>
2019-09-30 15:10:08 +0200

    tests: Move some ping checks to unit tests
    I had never added unit tests to the event processors because (in
    addition to lack of time) I found it weird to have them next to the unit
    tests for the Azafea core.
    
    Now that the tests are hosted by the event processors, it becomes
    obvious to add unit tests for them.
    
    This actually moves some integration tests to unit tests where it makes
    sense, which makes the `test-all` test suite faster and improves the
    code coverage of the unit tests.

---
+++ azafea/event_processors/ping/tests/test_ping_parsing.py
@@ -0,0 +1,44 @@
+# Copyright (c) 2019 - Endless
+#
+# This file is part of Azafea
+#
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
+
+from datetime import datetime, timezone
+import json
+
+
+def test_from_bytes():
+    from azafea.event_processors.ping.v1 import Ping
+
+    created_at = datetime.utcnow().replace(tzinfo=timezone.utc)
+    ping = Ping.from_serialized(json.dumps({
+        'release': 'release',
+        'count': 43,
+        'created_at': created_at.strftime('%Y-%m-%d %H:%M:%S.%fZ'),
+    }).encode('utf-8'))
+
+    assert ping.release == 'release'
+    assert ping.count == 43
+
+    # SQLAlchemy only transforms the string into a datetime when querying from the DB
+    assert ping.created_at == created_at.strftime('%Y-%m-%d %H:%M:%S.%fZ')
+
+
+def test_from_bytes_missing_count():
+    from azafea.event_processors.ping.v1 import Ping
+
+    created_at = datetime.utcnow().replace(tzinfo=timezone.utc)
+    ping = Ping.from_serialized(json.dumps({
+        'release': 'release',
+        'created_at': created_at.strftime('%Y-%m-%d %H:%M:%S.%fZ'),
+    }).encode('utf-8'))
+
+    assert ping.release == 'release'
+    assert ping.count == 0
+
+    # SQLAlchemy only transforms the string into a datetime when querying from the DB
+    assert ping.created_at == created_at.strftime('%Y-%m-%d %H:%M:%S.%fZ')
