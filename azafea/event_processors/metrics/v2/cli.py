dd1b1a6ecae4f7c918659aaf953bd83b7c6aed76
Mathieu Bridon <mathieu@hashbang.fr>
2019-10-16 15:56:26 +0200

    metrics: Allow renormalizing existing vendors
    This is implemented as a metrics-specific subcommand.
    
    Only one metrics event needs this though.
    
    Fixes #23

---
+++ azafea/event_processors/metrics/v2/cli.py
@@ -0,0 +1,69 @@
+# Copyright (c) 2019 - Endless
+#
+# This file is part of Azafea
+#
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
+import argparse
+import logging
+
+from sqlalchemy.orm.session import Session as DbSession
+
+from azafea.config import Config
+from azafea.model import Db
+from azafea.utils import progress
+from azafea.vendors import normalize_vendor
+
+from ..events import UpdaterBranchSelected
+
+
+log = logging.getLogger(__name__)
+
+
+def register_commands(subs: argparse._SubParsersAction) -> None:
+    normalize_vendors = subs.add_parser('normalize-vendors',
+                                        help='Normalize the vendors in existing records')
+    normalize_vendors.set_defaults(subcommand=do_normalize_vendors)
+
+
+def _normalize_chunk(dbsession: DbSession, start: int, stop: int) -> None:
+    records = dbsession.query(UpdaterBranchSelected).order_by(UpdaterBranchSelected.id)
+
+    for record in records.slice(start, stop):
+        vendor = normalize_vendor(record.hardware_vendor)
+
+        if vendor == record.hardware_vendor:
+            continue
+
+        record.hardware_vendor = vendor
+        dbsession.add(record)
+
+
+def do_normalize_vendors(config: Config, args: argparse.Namespace) -> None:
+    CHUNK_SIZE = 5000
+    db = Db(config.postgresql.host, config.postgresql.port, config.postgresql.user,
+            config.postgresql.password, config.postgresql.database)
+
+    log.info('Normalizing the vendors for "updater branch selected" (%s) events',
+             UpdaterBranchSelected.__event_uuid__)
+
+    with db as dbsession:
+        num_records = dbsession.query(UpdaterBranchSelected).count()
+
+    if num_records == 0:
+        log.info('-> No "updater branch selected" record in database')
+        return None
+
+    for i in range(0, num_records, CHUNK_SIZE):
+        stop = min(i + CHUNK_SIZE, num_records)
+
+        with db as dbsession:
+            _normalize_chunk(dbsession, i, stop)
+
+        progress(stop, num_records)
+
+    progress(num_records, num_records, end='\n')
+
+    log.info('All done!')
