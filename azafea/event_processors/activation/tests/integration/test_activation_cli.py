ce402adccf2e365524ed0a5adc742bb5409f767c
Mathieu Bridon <mathieu@hashbang.fr>
2019-10-16 15:56:26 +0200

    activation: Allow renormalizing existing vendors
    This is implemented as an activation-specific subcommand.
    
    Fixes #23

---
+++ azafea/event_processors/activation/tests/integration/test_activation_cli.py
@@ -0,0 +1,89 @@
+# Copyright (c) 2019 - Endless
+#
+# This file is part of Azafea
+#
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
+
+from datetime import datetime, timezone
+
+from azafea.tests.integration import IntegrationTest
+from azafea.vendors import normalize_vendor
+
+
+class TestActivation(IntegrationTest):
+    handler_module = 'azafea.event_processors.activation.v1'
+
+    def test_normalize_no_vendors(self, capfd):
+        from azafea.event_processors.activation.v1.handler import Activation
+
+        # Create the table
+        self.run_subcommand('initdb')
+        self.ensure_tables(Activation)
+
+        with self.db as dbsession:
+            assert dbsession.query(Activation).count() == 0
+
+        self.run_subcommand('test_normalize_no_vendors', 'normalize-vendors')
+
+        with self.db as dbsession:
+            assert dbsession.query(Activation).count() == 0
+
+        capture = capfd.readouterr()
+        assert 'No activation record in database' in capture.out
+
+    def test_normalize_existing_vendor(self):
+        from azafea.event_processors.activation.v1.handler import Activation
+
+        # Create the table
+        self.run_subcommand('initdb')
+        self.ensure_tables(Activation)
+
+        # Insert an activation with a known bad vendor
+        created_at = datetime.utcnow().replace(tzinfo=timezone.utc)
+        bad_vendor = 'EnDlEsS'
+
+        with self.db as dbsession:
+            dbsession.add(Activation(image='image', product='product', release='release',
+                                     country='HKG', created_at=created_at, vendor=bad_vendor))
+
+        with self.db as dbsession:
+            activation = dbsession.query(Activation).one()
+            assert activation.vendor == bad_vendor
+
+        # Normalize the activation vendors
+        self.run_subcommand('test_normalize_existing_vendor', 'normalize-vendors')
+        good_vendor = normalize_vendor(bad_vendor)
+
+        with self.db as dbsession:
+            activation = dbsession.query(Activation).one()
+            assert activation.vendor == good_vendor
+
+    def test_normalize_already_normalized_vendor(self):
+        from azafea.event_processors.activation.v1.handler import Activation
+
+        # Create the table
+        self.run_subcommand('initdb')
+        self.ensure_tables(Activation)
+
+        # Insert an activation with a known normalized vendor
+        created_at = datetime.utcnow().replace(tzinfo=timezone.utc)
+        vendor = 'Endless'
+        assert vendor == normalize_vendor(vendor)
+
+        with self.db as dbsession:
+            dbsession.add(Activation(image='image', product='product', release='release',
+                                     country='HKG', created_at=created_at, vendor=vendor))
+
+        with self.db as dbsession:
+            activation = dbsession.query(Activation).one()
+            assert activation.vendor == vendor
+
+        # Normalize the activation vendors
+        self.run_subcommand('test_normalize_already_normalized_vendor', 'normalize-vendors')
+
+        with self.db as dbsession:
+            activation = dbsession.query(Activation).one()
+            assert activation.vendor == vendor
