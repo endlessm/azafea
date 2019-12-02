bf7696e46f64d0c1e325d12848e7b23682c2a8d1
Mathieu Bridon <mathieu@hashbang.fr>
2019-12-02 17:22:50 +0100

    ping: Reuse the chunked query helper

--- azafea/event_processors/ping/v1/cli.py
+++ azafea/event_processors/ping/v1/cli.py
@@ -10,9 +10,10 @@ import argparse
 import logging
 
 from sqlalchemy.orm.exc import NoResultFound
+from sqlalchemy.orm.query import Query
 
 from azafea.config import Config
-from azafea.model import Db, DbSession
+from azafea.model import Db
 from azafea.utils import progress
 from azafea.vendors import normalize_vendor
 
@@ -28,11 +29,11 @@ def register_commands(subs: argparse._SubParsersAction) -> None:
     normalize_vendors.set_defaults(subcommand=do_normalize_vendors)
 
 
-def _normalize_chunk(dbsession: DbSession, start: int, stop: int) -> None:
-    ping_configs = dbsession.query(PingConfiguration).order_by(PingConfiguration.id)
-    pings = dbsession.query(Ping)
+def _normalize_chunk(chunk: Query) -> None:
+    ping_configs = chunk.session.query(PingConfiguration)
+    pings = chunk.session.query(Ping)
 
-    for record in ping_configs.slice(start, stop):
+    for record in chunk:
         vendor = normalize_vendor(record.vendor)
 
         if vendor == record.vendor:
@@ -46,7 +47,7 @@ def _normalize_chunk(dbsession: DbSession, start: int, stop: int) -> None:
         except NoResultFound:
             # There isn't, so lets just normalize the vendor
             record.vendor = vendor
-            dbsession.add(record)
+            chunk.session.add(record)
 
             continue
 
@@ -57,7 +58,7 @@ def _normalize_chunk(dbsession: DbSession, start: int, stop: int) -> None:
         assert pings.filter_by(config_id=record.id).count() == 0
 
         # … and delete the now unused ping configuration
-        dbsession.delete(record)
+        chunk.session.delete(record)
 
 
 def do_normalize_vendors(config: Config, args: argparse.Namespace) -> None:
@@ -68,19 +69,17 @@ def do_normalize_vendors(config: Config, args: argparse.Namespace) -> None:
     log.info('Normalizing the vendors for ping configurations')
 
     with db as dbsession:
-        num_records = dbsession.query(PingConfiguration).count()
+        query = dbsession.chunked_query(PingConfiguration, chunk_size=CHUNK_SIZE)
+        num_records = query.count()
 
-    if num_records == 0:
-        log.info('-> No ping configuration record in database')
-        return None
+        if num_records == 0:
+            log.info('-> No ping configuration record in database')
+            return None
 
-    for i in range(0, num_records, CHUNK_SIZE):
-        stop = min(i + CHUNK_SIZE, num_records)
-
-        with db as dbsession:
-            _normalize_chunk(dbsession, i, stop)
-
-        progress(stop, num_records)
+        for chunk_number, chunk in enumerate(query, start=1):
+            _normalize_chunk(chunk)
+            dbsession.commit()
+            progress(chunk_number * CHUNK_SIZE, num_records)
 
     progress(num_records, num_records, end='\n')
 
