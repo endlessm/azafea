e837c3dc81841b546b1409635f0f0914a0cd5300
Mathieu Bridon <mathieu@hashbang.fr>
2019-12-03 11:49:13 +0100

    metrics: Allow controlling the size of chunks to renormalize

--- azafea/event_processors/metrics/v2/cli.py
+++ azafea/event_processors/metrics/v2/cli.py
@@ -25,6 +25,8 @@ log = logging.getLogger(__name__)
 def register_commands(subs: argparse._SubParsersAction) -> None:
     normalize_vendors = subs.add_parser('normalize-vendors',
                                         help='Normalize the vendors in existing records')
+    normalize_vendors.add_argument('--chunk-size', type=int, default=5000,
+                                   help='The size of the chunks to operate on')
     normalize_vendors.set_defaults(subcommand=do_normalize_vendors)
 
 
@@ -40,7 +42,6 @@ def _normalize_chunk(chunk: Query) -> None:
 
 
 def do_normalize_vendors(config: Config, args: argparse.Namespace) -> None:
-    CHUNK_SIZE = 5000
     db = Db(config.postgresql.host, config.postgresql.port, config.postgresql.user,
             config.postgresql.password, config.postgresql.database)
 
@@ -48,7 +49,7 @@ def do_normalize_vendors(config: Config, args: argparse.Namespace) -> None:
              UpdaterBranchSelected.__event_uuid__)
 
     with db as dbsession:
-        query = dbsession.chunked_query(UpdaterBranchSelected, chunk_size=CHUNK_SIZE)
+        query = dbsession.chunked_query(UpdaterBranchSelected, chunk_size=args.chunk_size)
         num_records = query.count()
 
         if num_records == 0:
@@ -58,7 +59,7 @@ def do_normalize_vendors(config: Config, args: argparse.Namespace) -> None:
         for chunk_number, chunk in enumerate(query, start=1):
             _normalize_chunk(chunk)
             dbsession.commit()
-            progress(chunk_number * CHUNK_SIZE, num_records)
+            progress(chunk_number * args.chunk_size, num_records)
 
     progress(num_records, num_records, end='\n')
 
