dbbe8113a85d81ef68a9a61f7de0b09a9fb95b37
Mathieu Bridon <mathieu@hashbang.fr>
2019-12-03 13:52:59 +0100

    Add some missing copyright/license headers

--- azafea/event_processors/metrics/v2/__init__.py
+++ azafea/event_processors/metrics/v2/__init__.py
@@ -1,3 +1,12 @@
+# Copyright (c) 2019 - Endless
+#
+# This file is part of Azafea
+#
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
+
 # Reexport these so Azafea finds them
 from .cli import register_commands  # noqa: F401
 from .handler import process  # noqa: F401
