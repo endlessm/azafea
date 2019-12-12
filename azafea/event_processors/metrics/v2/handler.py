99ab95dfee3ab20cfe9baa8c817bc7fe960e9902
Mathieu Bridon <mathieu@hashbang.fr>
2019-12-12 15:06:23 +0100

    metrics: Remove the import handler
    It served its purpose well. We are now deployed in production, all the
    historical dfata has been imported, we will not need to import anything
    any more.

--- azafea/event_processors/metrics/v2/handler.py
+++ azafea/event_processors/metrics/v2/handler.py
@@ -20,14 +20,11 @@ from ..request import RequestBuilder
 log = logging.getLogger(__name__)
 
 
-def do_process(dbsession: DbSession, record: bytes, discard_serialized: bool = False) -> None:
+def process(dbsession: DbSession, record: bytes) -> None:
+    log.debug('Processing metric v2 record: %s', record)
+
     request_builder = RequestBuilder.parse_bytes(record)
     request = request_builder.build_request()
-
-    # FIXME: Remove this once the import is successful
-    if discard_serialized:
-        request.serialized = None
-
     dbsession.add(request)
 
     try:
@@ -58,9 +55,3 @@ def do_process(dbsession: DbSession, record: bytes, discard_serialized: bool = F
 
         if sequence_event is not None:
             log.debug('Inserting sequence event:\n%s', sequence_event)
-
-
-def process(dbsession: DbSession, record: bytes) -> None:
-    log.debug('Processing metric v2 record: %s', record)
-
-    do_process(dbsession, record)
