5fe2ed75a08a1d0f7a0c5a4931c11df54c5e2447
Guillaume Ayoub <guillaume@courtbouillon.org>
2020-09-30 10:34:36 +0200

    Update Sphinx configuration to allow code documentation

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -10,9 +10,11 @@
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
-# import os
-# import sys
-# sys.path.insert(0, os.path.abspath('.'))
+import sys
+from pathlib import Path
+
+sys.path.append(str(Path(__file__).parent.parent.parent))
+sys.path.append(str(Path(__file__).parent))
 
 
 # -- Project information -----------------------------------------------------
@@ -28,6 +30,8 @@ author = 'Mathieu Bridon'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
+    'sphinx.ext.autodoc',
+    'tables',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
@@ -41,6 +45,10 @@ exclude_patterns = []
 # Root file used as documentation index.
 master_doc = 'index'
 
+# If true, the current module name will be prepended to all description
+# unit titles (such as .. function::).
+add_module_names = False
+
 
 # -- Options for HTML output -------------------------------------------------
 
@@ -53,3 +61,11 @@ html_theme = 'alabaster'
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+
+
+# -- Options for extensions --------------------------------------------------
+
+# This value selects if automatically documented members are sorted
+# alphabetical (value 'alphabetical'), by member type (value 'groupwise') or by
+# source order (value 'bysource'). The default is alphabetical.
+autodoc_member_order = 'bysource'
