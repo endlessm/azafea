116d4ca218d4d297e206789211bb04238626b7da
Guillaume Ayoub <guillaume@courtbouillon.org>
2021-05-20 16:39:15 +0200

    Parse events

--- azafea/event_processors/endless/metrics/v3/model/_channel.py
+++ azafea/event_processors/endless/metrics/v3/model/_channel.py
@@ -7,8 +7,25 @@
 # file, You can obtain one at https://mozilla.org/MPL/2.0/.
 
 
+from sqlalchemy.dialects.postgresql import JSONB
+from sqlalchemy.schema import Column
+from sqlalchemy.types import Boolean, Integer, Unicode
+
 from azafea.model import Base
 
 
 class Channel(Base):
     __tablename__ = 'channel_v3'
+
+    id = Column(Integer, primary_key=True)
+
+    #: image ID (e.g. ``eos-eos3.1-amd64-amd64.170115-071322.base``)
+    image_id = Column(Unicode, nullable=False)
+    #: dictionary of string keys (such as ``facility``, ``city`` and
+    #: ``state``) to the values provided in the location.conf file (written by
+    #: the ``eos-label-location`` utility)
+    site = Column(JSONB, nullable=False)
+    #: dual boot computer
+    dual_boot = Column(Boolean, nullable=False)
+    #: live USB stick
+    live_usb = Column(Boolean, nullable=False)
