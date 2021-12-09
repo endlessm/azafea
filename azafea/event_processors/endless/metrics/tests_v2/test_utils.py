adc7386928c811b66f408ed13450ed3b4ace8351
Dan Nicholson <dbn@endlessos.org>
2021-12-09 10:06:37 -0700

    metrics: Make clamp_to_int64 print a traceback
    Simply using `log.error` to say that the provided value exceeds the
    int64 max isn't that helpful since it doesn't tell you where that value
    came from.
    
    Instead, raise an error and use `log.exception` so that a traceback is
    printed. This will show in much more detail (maybe too much) where this
    value came from so we can more easily find the problem.
    
    Unfortunately, this utility function is duplicated for both v2 and v3
    metrics event though there's nothing metrics specific about it. A later
    fix should unify some of these functions.
    
    https://phabricator.endlessm.com/T32790

--- azafea/event_processors/endless/metrics/tests_v2/test_utils.py
+++ azafea/event_processors/endless/metrics/tests_v2/test_utils.py
@@ -9,6 +9,7 @@
 
 from datetime import datetime, timezone
 from gi.repository import GLib
+import logging
 
 import pytest
 
@@ -47,3 +48,30 @@ def test_get_event_datetime():
 
     assert get_event_datetime(1536122990000000000, 20000000000, 30000000000) \
         == datetime(2018, 9, 5, 4, 50, tzinfo=timezone.utc)
+
+
+def test_clamp_to_int64(caplog):
+    from azafea.event_processors.endless.metrics.v2.utils import clamp_to_int64, log as utils_log
+
+    val = clamp_to_int64(GLib.MAXUINT64)
+    assert val == GLib.MAXINT64
+    assert caplog.records != []
+
+    # Check that the message and exception come through.
+    record = caplog.records[0]
+    assert record.name == utils_log.name
+    assert record.levelno == logging.ERROR
+    assert record.msg == f'Clamped integer larger than MAXINT64: {GLib.MAXUINT64}'
+    assert record.exc_info is not None
+    assert record.exc_info[0] == ValueError
+    assert str(record.exc_info[1]) == f'{GLib.MAXUINT64} larger than {GLib.MAXINT64}'
+
+    caplog.clear()
+    val = clamp_to_int64(GLib.MAXINT64)
+    assert val == GLib.MAXINT64
+    assert caplog.records == []
+
+    caplog.clear()
+    val = clamp_to_int64(GLib.MAXINT64 + 1)
+    assert val == GLib.MAXINT64
+    assert caplog.records != []
